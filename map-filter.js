09ead1ae8c698628775edeecea105eaa5b2261f3
rahat3739 <snrahat.stud4143@gmail.com>
2021-02-02 20:36:03 +0600

    map , filter niey mota moti ektah darona

--- map-filter.js
+++ map-filter.js
@@ -7,13 +7,20 @@
 // }
 //  console.log(output);
 
- const numbers = [3, 4, 6, 2, 8];
- let output = [];
- function square(element){
-    return element * element;
- }
- numbers.map(function(element){
-     console.log(element);
- });
- let result = square(6, 5);
-console.log(result);
+ const numbers = [3, 4, 6, 9, 8];
+//  let output = [];
+//  function square(element){
+//     return element * element;
+//   }
+//  const result = numbers.map(function(element){
+//      return element * element ;
+//  });
+
+// const result = numbers.map(x => x*x);
+//  console.log(result);
+
+// const bigger = numbers.filter(x => x< 5);
+// console.log(bigger);
+
+const isThere = numbers.find(x => x>5);
+console.log(isThere);
---
+++ scope.js
@@ -0,0 +1,18 @@
+
+let bonus = 20;
+function sum(num1, num2){
+    let result = num1 + num2 + bonus;
+    // console.log(bonus);
+    if( result > 15){
+        var mood = "happy";
+        mood = "sad"
+        mood = "franky"
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+
+}
+let Sum = sum(12, 28);
+// console.log(bonus);
+// console.log(Sum);
